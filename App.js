397d23ddfd8030b9b08f8a13ca45111c3902d964
Aman Maharjan <mhrjn.aman@gmail.com>
2020-12-07 15:24:25 +0545

    Design intro

--- App.js
+++ App.js
@@ -1,23 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import Button from './src/Button';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, View, SafeAreaView } from "react-native";
+
+import HomeScreen from "./src/screens/home/HomeScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>hello world!!
-      </Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View
+        style={{ backgroundColor: "yellow", flex: 3, margin: 10, padding: 10 }}
+      >
+        <View style={{ backgroundColor: "teal", width: 100, height: 100 }} />
+      </View>
+
+      <View style={{ backgroundColor: "green", flex: 1 }} />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "red",
   },
 });
